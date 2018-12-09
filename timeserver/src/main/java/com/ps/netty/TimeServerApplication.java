9f9e54945826ff015c0b598c2dfcee948c953823
jungle0127 <arbor@foxmail.com>
2018-12-09 13:54:01 +0800

    added netty demo project frame

---
+++ timeserver/src/main/java/com/ps/netty/TimeServerApplication.java
@@ -0,0 +1,11 @@
+package com.ps.netty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TimeServerApplication.class,args);
+    }
+}
